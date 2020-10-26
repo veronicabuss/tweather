f46559c28807df25541fe6055489389ddedae4f6
Veronica <veronicabuss121@gmail.com>
2020-10-25 20:11:37 -0400

    Added bootstrap styling and choose a city input

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 const routerOptions = [
   { path: '/', component: 'Home' },
