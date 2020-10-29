658a70a51a1e90f62d8be71dd961c1aef8fc9463
kingogruffner <garrettruffner42@gmail.com>
2020-10-29 02:45:49 -0400

    :tada: Update helper script

--- startFront.sh
+++ startFront.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if [ -z "${VIRTUAL_ENV}" ]; then
-  echo "You must activate your virtual environment before running this script. Use: source backend/venv/bin/activate"
-else
-  npm run dev --prefix ./frontend
-fi
+npm run dev --prefix ./frontend
