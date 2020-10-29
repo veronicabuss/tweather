7ce10b08a338ba9274e142bfe710d0af37d0077e
kingogruffner <garrettruffner42@gmail.com>
2020-10-29 02:48:09 -0400

    :tada: Update helper script

--- startBack.sh
+++ startBack.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "${VIRTUAL_ENV}" ]; then
-  echo "You must activate your virtual environment before running this script. Use: source backent/venv/bin/activate"
+  echo "You must activate your virtual environment before running this script. Use: source backend/venv/bin/activate"
 else
   flask run
 fi
