21d65109b7212fdbeb5fffb18aff4a5af3130afb
kingogruffner <garrettruffner42@gmail.com>
2020-11-01 17:21:44 -0500

    :memo: update requirements

--- startBack.sh
+++ startBack.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 if [ -z "${VIRTUAL_ENV}" ]; then
-  echo "You must activate your virtual environment before running this script. Use: source backend/venv/bin/activate"
+	echo "You must activate your virtual environment before running this script. Use: source backend/venv/bin/activate"
 else
-  flask run
+	export FLASK_APP=backend/run.py 
+	export FLASK_DEBUG=1
+	flask run
 fi
